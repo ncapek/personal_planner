03060fa976c65af77ae817d9a22e1c7b8958f1d6
ncapek <capekn@centrum.cz>
2023-12-23 00:45:19 +0100

    fitness api cleanup

--- fitness.py
+++ fitness.py
@@ -1,43 +1,94 @@
+"""
+Fitness Data Module.
+
+This module provides a class-based interface to fetch and process fitness-related data. It encapsulates functionalities
+to retrieve various health metrics and offers clean, processed data suitable for further analysis or display.
+
+Classes:
+    FitnessAPI: A class to interact with a specified fitness data API.
+
+Example:
+    fitness_api = FitnessAPI(NOCODE_API_LINK)
+    fitness_api.get_clean_fitness_data()
+
+Note:
+    Requires an API endpoint to fetch fitness data.
+"""
+
 import requests
+import logging
 from config import NOCODE_API_LINK
 
-def fetch_health_data(url: str, params: dict) -> dict:
-    """
-    Fetch health data from the given URL.
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-    :param url: API endpoint URL.
-    :param params: Parameters for the GET request.
-    :return: JSON response as a dictionary.
-    """
-    try:
-        response = requests.get(url=url, params=params)
-        response.raise_for_status()  # Raises HTTPError for bad requests
-        return response.json()
-    except requests.exceptions.HTTPError as e:
-        print(f"HTTP error: {e}")
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+class FitnessAPI:
+    def __init__(self, api_url: str):
+        """
+        Initializes the FitnessAPI with the provided API URL.
 
-def extract_relevant_data(result: dict) -> dict:
-    """
-    Extracts relevant key-value pairs from the JSON response.
+        Args:
+            api_url (str): The API endpoint URL for fetching fitness data.
+        """
+        self.api_url = api_url
 
-    :param result: The JSON response as a dictionary.
-    :return: Dictionary with extracted key-value pairs.
-    """
-    extracted_data = {}
-    for key in ['steps_count', 'weight', 'active_minutes', 'calories_expended', 'heart_minutes', 'sleep_segment', 'activity_summary']:
-        if key in result:
-            if len(result[key]) == 1:
+    def fetch_health_data(self, params: dict = {}) -> dict:
+        """
+        Fetches health data from the API.
+
+        Args:
+            params (dict): Parameters for the GET request.
+
+        Returns:
+            dict: JSON response as a dictionary. Returns an empty dict if an error occurs.
+        """
+        try:
+            response = requests.get(url=self.api_url, params=params)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.HTTPError as e:
+            logger.error(f"HTTP error: {e}")
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Error: {e}")
+        return {}
+
+    def extract_relevant_data(self, result: dict) -> dict:
+        """
+        Extracts relevant key-value pairs from the JSON response.
+
+        Args:
+            result (dict): The JSON response as a dictionary.
+
+        Returns:
+            dict: Dictionary with extracted key-value pairs.
+        """
+        extracted_data = {}
+        keys_of_interest = ['steps_count', 'weight', 'active_minutes', 'calories_expended', 'heart_minutes', 'sleep_segment', 'activity_summary']
+        for key in keys_of_interest:
+            if key in result and len(result[key]) == 1:
                 extracted_data[key] = result[key][0]['value']
-    return extracted_data
+        return extracted_data
+
+    def get_clean_fitness_data(self, params: dict = {}) -> dict:
+        """
+        Fetches, processes, and returns clean and formatted fitness data.
+
+        This method combines the process of fetching fitness data and formatting it into a more readable and useful format.
+
+        Args:
+            params (dict): Parameters for the GET request to the fitness data API.
+
+        Returns:
+            dict: A dictionary containing formatted fitness data. Returns an empty dict if fetching fails or data is incomplete.
+        """
+        raw_data = self.fetch_health_data(params)
+        if not raw_data:
+            return {}
+
+        return self.extract_relevant_data(raw_data)
 
 if __name__ == "__main__":
-    # Example usage
-    url = NOCODE_API_LINK
-    params = {}
-    result = fetch_health_data(url, params)
-
-    if result:
-        relevant_data = extract_relevant_data(result)
-        print(relevant_data)
+    fitness_api = FitnessAPI(NOCODE_API_LINK)
+    result = fitness_api.get_clean_fitness_data()
+    print(result)
